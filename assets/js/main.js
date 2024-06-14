7ac3361cb011eefebd5e7b26a9cfed3d447c3701
Dinesh18042001 <dinesh@1804200182@gmail.com>
2024-06-14 16:39:10 +0530

    updated code

--- assets/js/main.js
+++ assets/js/main.js
@@ -812,3 +812,29 @@
       );
     });
 })(jQuery);
+
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: true,
+      dots: true,
+      responsive: {
+          0: {
+              items: 1
+          },
+          600: {
+              items: 2
+          },
+          1000: {
+              items: 3
+          }
+      },
+      navText: ["<button>&lt;</button>", "<button>&gt;</button>"] // Customize the nav buttons as needed
+  });
+});
+
+
+
+
